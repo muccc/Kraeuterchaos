0f96aa0e212e304895d6cad4d7e159b422228fc8
Jenny Hofbauer <j.hofbauer223@t-online.de>
2018-09-12 22:01:53 +0200

    Pump
    + Pump

--- kraeuterkasten/kraeuterkasten.ino
+++ kraeuterkasten/kraeuterkasten.ino
@@ -5,29 +5,31 @@ int soilMoistureValue = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(8, OUTPUT);
+  pinMode(8, OUTPUT); //LED
+  pinMode(7, OUTPUT); //PUMP
 }
 
 
 void loop() {
-  digitalWrite(8, LOW); 
-  water();
   
-}
-
-void water() {
-soilMoistureValue = analogRead(A0); 
+  digitalWrite(7, HIGH);
+  soilMoistureValue = analogRead(A0); 
 if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
 {
   Serial.println("Very Wet");
+  digitalWrite(7, HIGH);
 }
 else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
 {
   Serial.println("Wet");
+  digitalWrite(7, HIGH);
 }
 else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
 {
-  Serial.println("Dry");
+   Serial.println("Dry");
+  digitalWrite(7, LOW);
 }
-delay(10000);
+delay(1000);
 }
+
+
